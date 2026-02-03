e1b4e0cca12563c18c3ddb23b0dffaa7ade71d8f
smilesun <smilesun.east@gmail.com>
2026-02-03 11:57:22 +0100

    .

---
+++ install_system_dependencies.sh
@@ -0,0 +1,2 @@
+sudo apt-get update                                                                                                                                
+sudo apt-get install -y libeigen3-dev
